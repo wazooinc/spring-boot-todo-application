d6589053101b6ed0844be72ee2875ab0c9cda3f4
Erik Yuzwa <erikyuzwa@gmail.com>
2022-06-05 15:20:12 -0600

    remove unused

--- src/main/java/com/wazooinc/springboottodoapplication/controllers/TodoFormController.java
+++ src/main/java/com/wazooinc/springboottodoapplication/controllers/TodoFormController.java
@@ -3,8 +3,6 @@ package com.wazooinc.springboottodoapplication.controllers;
 import com.wazooinc.springboottodoapplication.models.TodoItem;
 import com.wazooinc.springboottodoapplication.repositories.TodoItemRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,8 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class TodoFormController {
     
-    private final Logger logger = LoggerFactory.getLogger(TodoFormController.class);
-
     @Autowired
     private TodoItemRepository todoItemRepository;
 
